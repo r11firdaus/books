3f1f823f06ead9a2e345ed84e7b2e1a6d2e17698
Reza Firdaus <azerus98@gmail.com>
2023-09-06 13:34:02 +0700

    add username to user

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
   belongs_to :author
+  validates :title, :author_id, :date_release, presence: :true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   resources :books
   resources :authors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "books#index"
+  devise_for :users, :controllers => {
+    registrations: 'registrations'
+  }
 end
